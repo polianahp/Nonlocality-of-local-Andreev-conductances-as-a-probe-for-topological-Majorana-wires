ba6c85ce4b74885dfe4aa97a8ca150da9ce06baa
Nathan <nateosmith81@gmail.com>
2026-02-21 14:41:58 -0500

    file run

--- main_parallel.py
+++ main_parallel.py
@@ -172,14 +172,14 @@ if __name__ == "__main__":
     
     
     ## setting up different tests
-    #V0 = 0.0  * Delta 
-    #dirname = 'new_corr_clean_dis_test'
+    V0 = 0.0  * Delta 
+    dirname = 'reruncorr_clean_dis_test'
     
     #V0 = 3.5  * Delta 
     #dirname = 'new_corr_med_dis_test'    #corr_med_dis_test
     
-    V0 = 10.5 * Delta 
-    dirname = 'newrange_corr_stong_dis_test'  #corr_stong_dis_test
+    #V0 = 10.5 * Delta 
+    #dirname = 'reruncorr_stong_dis_test'  #corr_stong_dis_test
 
     Upoints = 50 
     num_engs = 101 
