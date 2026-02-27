33d7747f8af9ac6cb9eddd78ae7e377409ffa9c5
Nathan <nateosmith81@gmail.com>
2026-02-26 19:31:43 -0500

    file name change

--- main_parallel.py
+++ main_parallel.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
     #dirname = 'new_corr_med_dis_test'    #corr_med_dis_test
     
     V0 = 10.5 * Delta 
-    dirname = 'nobarriers_stong_dis_test'  #corr_stong_dis_test
+    dirname = 'barriers_stong_dis_test'  #corr_stong_dis_test
 
     Upoints = 50 
     num_engs = 101  
